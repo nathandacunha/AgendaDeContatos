467e8d1b15cd3c33ce659c3e65b2815031eb40af
nathandacunha <nathandacunha233@gmail.com>
2026-05-26 21:17:47 -0300

    feat: adicionei um menu de interação

--- main.c
+++ main.c
@@ -1,7 +1,41 @@
 #include <stdio.h>
 
 int main() {
-    printf("Primeiro commit");
+    int opcao, idContato = 0;
+
+    while(opcao != 6) {
+        printf("Bem-vindo(a) à Agenda de Contatos\n");
+        printf("[1] - Adicionar novo contato \n");
+        printf("[2] - Excluir um contato \n");
+        printf("[3] - Alterar um contato \n");
+        printf("[4] - Listar todos os contatos existentes\n");
+        printf("[5] - Localizar um contato\n");
+        printf("[6] - Sair");
+        scanf("%d", &opcao);
+
+        switch(opcao) {
+            case 1:
+                // lógica de adicionar um novo contato
+                break;
+            case 2:
+                // excluir um contato
+                break;
+            case 3:
+                // alterar um contato
+                break;
+            case 4:
+                // listar todos os contatos
+                break;
+            case 5:
+                // localizar um contato
+                break;
+            case 6:
+                break;
+            default: 
+                printf("Erro!");
+                break;
+        }
+    }
 
     return 0;
 }
